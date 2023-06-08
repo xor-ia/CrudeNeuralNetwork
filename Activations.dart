e6307ec50b8ed0834d3d0b11a2f737ee3f4af347
xor ia <kimsiri1710@gmail.com>
2023-06-08 18:17:41 +0700

    ADD MORE ACTIVATION FUNCTIONS!

--- Activations.dart
+++ Activations.dart
@@ -17,33 +17,33 @@ double D_relu(double x) {
 	return x > 0 ? 1 : 0;
 }
 
-
-
-class Sigmoid extends Layer {
+class Activation extends Layer {
+	late double Function(double) ac_forward;
+	late double Function(double) ac_backward;
 	@override
 	Matrix forward(Matrix inp) {
 		this.input = inp.copy();
-		return inp.map(sigmoid);
+		return inp.map(this.ac_forward);
 	}
 
 	@override
 	Matrix backward(Matrix gradients, double lr) {
-		return this.input.map(D_sigmoid).mulElement(gradients);
+		return this.input.map(this.ac_backward).mulElement(gradients);
+	}
+}
+
+class Sigmoid extends Activation {
+	Sigmoid() {
+		this.ac_forward = sigmoid;
+		this.ac_backward = D_sigmoid;
 	}
 }
 
 
-class ReLU extends Layer {
-	@override
-	Matrix forward(Matrix inp) {
-		this.input = inp.copy();
-		return inp.map(relu);
-	}
-
-	@override
-	Matrix backward(Matrix gradients, double lr) {
-		return this.input.map(D_relu).mulElement(gradients);
+class ReLU extends Activation {
+	ReLU() {
+		this.ac_forward = relu;
+		this.ac_backward = D_relu;
 	}
 }
-
 
